c13ff0506eae8adcc4cce1a623b4fc4eb9764fce
Kuba <75746226+Whisperer8526@users.noreply.github.com>
2022-07-19 16:00:59 +0200

    Update metrix_tools.py

--- metrix_tools.py
+++ metrix_tools.py
@@ -9,3 +9,27 @@ def match_twitter_posts(links_file, export_file):
     matched_posts =  pd.merge(data, export, on='post_id')
     
     return matched_posts
+
+
+def get_clean_url(link: str):
+    fb_link_formats = ['facebook.com', 'fb.com']
+    for item in fb_link_formats:
+        if item in link:
+            url = urlopen(link, context=certificate)
+            page = BeautifulSoup(url.read(), 'lxml')
+            #sleep(0.1)
+            links = page.find_all('link', href=True)
+
+            link_list = []
+            for item in links:
+                if 'www.facebook.com' in item['href']:
+                    link_list.append(item['href'])
+            if len(link_list) == 0:
+                return link
+            if 'login/web' in link_list[0]:
+                return link
+            else:
+                return link_list[0]
+        
+        else:
+            return link

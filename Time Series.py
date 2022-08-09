4d46cbf79fc5a5d1013961b8ee92b5e12c871077
Kuba <75746226+Whisperer8526@users.noreply.github.com>
2022-08-09 12:53:35 +0200

    Update Time Series.py

--- Time Series.py
+++ Time Series.py
@@ -3,7 +3,9 @@ import statsmodels.api as sm
 from sklearn.cluster import DBSCAN
 
 def get_lag(x, y):
-    """Get lag between two timeseries. They need to have aligned datetime index"""
+    """
+    Get lag between two timeseries. They need to have aligned datetime index
+    """
 
     correlation = signal.correlate(data["closing_price_detrend"], 
                                data["volume_detrend"], 
@@ -13,3 +15,17 @@ def get_lag(x, y):
                                    mode="full")
     lag = lags[np.argmax(correlation)]
     return lag
+
+
+def detrend_timeseries_data(data):
+    """
+    Detrends data in all columns and add detrended data as new columns with '_detrend' suffix.
+    Requires datetime index.
+    """
+
+    for column in data.columns:
+        result = sm.tsa.seasonal_decompose(data[column])
+        detrend = data[column] - result.trend
+        data[f'{column}_detrend'] = detrend
+
+    return data.dropna()
